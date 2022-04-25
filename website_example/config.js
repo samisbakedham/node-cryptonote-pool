83f65cd191dd617f59772d68e13861c0ff96ff40
Samuel Safahi <samsafahi@gmail.com>
2022-04-25 16:28:56 -0700

    add

--- website_example/config.js
+++ website_example/config.js
@@ -1,17 +1,17 @@
-var api = "http://cryppit.com:8117";
+var api = "https://clericiproject.com:8117";
 
 var coinUnits = 1000000000000;
 
-var poolHost = "cryppit.com";
+var poolHost = "clericiproject.com";
 
 var irc = "irc.freenode.net/#monero-pools";
 
 var email = "support@cryppit.com";
 
-var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+var cryptonatorWidget = ["CLC-BTC", "CLC-USD", "CLC-EUR", "CLC-GBP"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/xmr/block/";
+var blockchainExplorer = "http://explorer.clericiproject.com/block/";
 
-var transactionExplorer = "http://chainradar.com/xmr/transaction/";
+var transactionExplorer = "http://explorer.clericiproject.com/transaction/";
